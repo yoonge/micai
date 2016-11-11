8624f6df0ee174a40d1d0f5dcb732ae1747c8f10
Yoonge <7884008@qq.com>
2016-11-11 18:49:21 +0800

    Update

---
+++ src/pages/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="vux-demo">
+    <img class="logo" src="../assets/logo@2x.png">
+    <h1>{{ msg }}</h1>
+  </div>
+  <div class="from-wrapper">
+    <group>
+      <cell title="Vux" value="Cool" is-link></cell>
+    </group>
+  </div>
+</template>
+
+<script>
+import Group from 'vux-components/group'
+import Cell from 'vux-components/cell'
+
+export default {
+  components: {
+    Group,
+    Cell
+  },
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello MiCai !'
+    }
+  }
+}
+</script>
+
+<style>
+.vux-demo {
+  text-align: center;
+}
+
+.logo {
+  width: 132px;
+  margin-top: 30px;
+}
+</style>
