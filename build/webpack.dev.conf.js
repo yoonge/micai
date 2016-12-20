86506afc4af217e508a7a2cea75a844c1c21f060
Yoonge <7884008@qq.com>
2016-12-20 18:40:03 +0800

    Update

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -34,9 +34,10 @@ function getEntry(globPath) {
   var entries = {}, basename, tmp, pathname
 
   glob.sync(globPath).forEach(function(entry) {
-    basename = path.basename(entry, path.extname(entry))
-    tmp = entry.split('/').splice(-3)
-    pathname = tmp.splice(0, 1) + '/' + basename // 正确输出js和html的路径
+    // basename = path.basename(entry, path.extname(entry))
+    // tmp = entry.split('/').splice(-3)
+    // pathname = tmp.splice(0, 1) + '/' + basename // 正确输出js和html的路径
+    pathname = path.basename(entry, path.extname(entry))
     entries[pathname] = entry
   });
   console.log("dev-entrys:")
--- config/index.js
+++ config/index.js
@@ -4,10 +4,11 @@ var path = require('path')
 module.exports = {
   build: {
     env: require('./prod.env'),
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../dist/login.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
+    // assetsPublicPath: '/portal/content/weixinPublic/vue/',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
